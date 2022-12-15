3ca0869fed7e5678025a809f3faba19970e706e7
Proxied <13905916@student.uts.edu.au>
2022-12-15 19:37:14 +1100

    Added 8ball slash command

--- main.py
+++ main.py
@@ -171,4 +171,17 @@ async def f1(interaction : Interaction):
     ]
     await interaction.response.send_message(f"Your choosen driver is: **{random.choice(f1drivers)}**", ephemeral=False)
 
+@bot.slash_command(name="8ball", description="Let 8Ball predict the future")
+async def ball(interaction : Interaction, question:str):
+    eightball = [
+            'It is certain.', 'It is decidedly so.', 'Without a doubt.', "I'm feeling well.", 'Yes - definitely.',
+            'You may rely on it.', 'As I see it yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.',
+            'Reply hazy, try again.', 'Better not tell you now.', 'Concentrate and ask again.', "Don't count on it.",
+            'I cannot predict now.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtfull.',
+            'I am tired. *proceeds with sleeping*.', 'As I see it, yes.', 'Yes.', 'Positive.', 'From my point of view, yes.',
+            'Convinced.', 'Most likley.', 'Chances point to high.', 'No.', 'Negative.', 'Not convinced.', 'Perhaps.',
+            'Not sure', 'Maybe', "I'm too lazy to predict."
+        ]
+    await interaction.response.send_message(f"Hey, {interaction.user.mention}!\n:8ball: *Question:* **{question}**\n:8ball: *Answer:* **{random.choice(eightball)}**", ephemeral=False)
+
 bot.run(os.getenv("TOKEN"))
